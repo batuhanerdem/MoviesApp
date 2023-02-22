dd9acf9ee5788cd100ed08ac8345d7dc22ef3a29
Batuhan Erdem <79407999+batuhanerdem@users.noreply.github.com>
2023-02-22 03:24:59 +0300

    ui

--- components/movie.js
+++ components/movie.js
@@ -2,22 +2,32 @@ import React from 'react';
 import {Image, Text, View} from "react-native";
 import {BASE_IMAGE_URL} from "../screens/HomeScreen";
 import PropTypes from 'prop-types';
+import {screen} from "../screens/HomeScreen";
+import {StyleSheet} from "react-native";
+
 
 const propTypes = {
     item: PropTypes.object,
 };
+
 class MovieView extends React.PureComponent {
     render() {
         const {item} = this.props
         return (
-            <View>
-               <Image source={{uri: BASE_IMAGE_URL + item.poster_path}}
-                      style={{width: 100, height: 100, padding: 20}}/>
+            <View style={{
+                marginHorizontal: 9,
+            }}>
+                <Image source={{uri: BASE_IMAGE_URL + item.poster_path}}
+                       style={styles.movieImage}/>
             </View>
         );
     }
 }
 
 MovieView.propTypes = propTypes;
+const styles = StyleSheet.create({
+        movieImage: {width: screen.width/3.6, height: screen.height / 5, borderRadius: 10}
+    }
+);
 
 export default MovieView;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import {getPopularMovies, getTopRatedMovies} from '../service/service';
 import {StatusBar} from "expo-status-bar";
 import React, {useEffect, useState} from "react";
-import {FlatList, StyleSheet, Text, View} from "react-native";
+import {FlatList, SafeAreaView, StyleSheet, Text, View, Dimensions} from "react-native";
 import {SliderBox} from 'react-native-image-slider-box';
 import MovieView from "../components/movie";
 
 export const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+export const screen = Dimensions.get("screen")
 const HomeScreen = () => {
     const [topRatedMovieUrl, setTopRatedMovieUrl] = useState([]);
     const [popularMovies, setPopularMovies] = useState([]);
@@ -37,28 +38,32 @@ const HomeScreen = () => {
     //autoplay true yap
 
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
 
             <View style={styles.slider}>
                 <SliderBox images={topRatedMovieUrl}
-                           sliderBoxHeight={400}
+                           sliderBoxHeight={screen.height/2}
                            resizeMode={'contain'}
                            dotStyle={styles.dot}
+                           ImageComponentStyle={styles.sliderImage}
                            autoplay={false}/>
                 {error && <ErrorText/>}
                 <StatusBar style="auto"/>
             </View>
 
             <Text style={styles.popularText}>Popüler Filmler</Text>
-            <View style={styles.list}>
+            <View style={styles.listContainer}>
                 <FlatList data={popularMovies}
                           horizontal={true}
+                          style={styles.flatList}
                           renderItem={(item) =>
                               <MovieView style={styles.container} item={item.item}/>
                           }/>
             </View>
+            {/*<View style={styles.listContainer}>*/}
+            {/*</View>*/}
 
-        </View>
+        </SafeAreaView>
     );
 }
 
@@ -69,22 +74,29 @@ const styles = StyleSheet.create({
         backgroundColor: "rgba(8,89,109,0.95)"
     },
     slider: {
-        flex: 1,
-        paddingTop: 80,
+        height: screen.height/2,
         alignItems: "center",
         justifyContent: "center",
     },
-    list: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+    sliderImage:{
+        borderRadius:10
+    },
+    listContainer: {
+        flex: 0,
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+        height: screen.height/5
+    },
+    flatList: {
+        marginHorizontal: 10
     },
     popularText: {
         fontSize: 20,
-        color: "#fff",
-        paddingStart: 10,
+        color: "#f7f720",
+        paddingStart: 19,
         fontWeight: "bold",
-        paddingTop: 10
+        paddingTop: 10,
+        paddingBottom:10
     },
     dot: {
         height: 0
